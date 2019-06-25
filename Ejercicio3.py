90e6704b0c795fa06b3467549e11bb60d175c5f6
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-25 08:28:26 -0500

    3

---
+++ Ejercicio3.py
@@ -0,0 +1,16 @@
+import random
+def usuario():
+    numeros = []
+    for i in range(1,7):
+        x = int(input("Ingrese 6 números entre 1 y 10 por favor: "))
+        numeros.append(x)
+        print(numeros)
+    print("Estos son sus numeros ", numeros)
+
+    
+usuario()
+
+
+
+
+    
